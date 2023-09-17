03047a8bbf05f6bbe07a2a014fd5e661dd0c61cc
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-09-17 14:26:50 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    SkillsComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/skills/skills.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss']
+})
+export class SkillsComponent {
+  icons= ['../../assets/img/iconsSkills/Angular.svg', 
+          '../../assets/img/iconsSkills/css.svg', 
+          '../../assets/img/iconsSkills/html.svg', 
+          '../../assets/img/iconsSkills/Firebase.svg',
+          '../../assets/img/iconsSkills/JavScript.svg', 
+          '../../assets/img/iconsSkills/Typescript.svg',
+          '../../assets/img/iconsSkills/Git.svg',
+          '../../assets/img/iconsSkills/Api.svg',
+          '../../assets/img/iconsSkills/mat des.svg',
+          '../../assets/img/iconsSkills/Scrum.svg',
+          '../../assets/img/iconsSkills/Automation.svg',
+  ];
+}
