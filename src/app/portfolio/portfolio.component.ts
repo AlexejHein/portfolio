8e8fc62d2dc05c7a219354e9970f752dbe910745
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2023-09-30 15:31:29 +0200

    add media

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as portfolioItems from '../../assets/portfolio-items.json';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,28 +8,7 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
-  portfolioItems = [
-    {
-      title: 'Join',
-      technologies: 'JavaScript | HTML | CSS',
-      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-      imageUrl: '../../assets/img/join.png',
-      links: [
-        { url: 'https://alexej-hein.developerakademie.net/join/index.html', label: 'Live test' },
-        { url: 'https://github.com/AlexejHein/join', label: 'Github', class: 'gitbtn' }
-      ]
-    },
-    {
-      title: 'Pollo Loco',
-      technologies: 'JavaScript | HTML | CSS',
-      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
-      imageUrl: '../../assets/img/PolloLoco.png',
-      links: [
-        { url: 'https://alexej-hein.developerakademie.net/ElPolloLoco', label: 'Live test' },
-        { url: 'https://github.com/AlexejHein/ElPolloLoco', label: 'Github', class: 'gitbtn' }
-      ]
-    },
-  ];
+  portfolioItems: any = (portfolioItems as any).default;
 
 
   goToLink(event: Event, url: string): void {
