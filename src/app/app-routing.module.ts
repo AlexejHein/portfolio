a1f8ae60cd92749781eeba03d95d1a801bc213c0
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-07-02 17:45:50 +0200

    bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import {PrivacyPolicyComponent} from "./privacy-policy/privacy-policy.component";
 
 const routes: Routes = [
   { path: 'menu', component: MenuComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent}
 ];
 
 @NgModule({
