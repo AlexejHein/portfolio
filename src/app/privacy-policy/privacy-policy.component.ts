e28d983a80c8a2c409e880c918bb61e8e20d0a5a
AlexejHein <134226688+AlexejHein@users.noreply.github.com>
2024-07-02 17:50:58 +0200

    bug fix

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,14 +1,19 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   templateUrl: './privacy-policy.component.html',
+  standalone: true,
   styleUrls: ['./privacy-policy.component.scss']
 })
 export class PrivacyPolicyComponent {
   @Output() close = new EventEmitter();
+
+  constructor(private router: Router) { }
   closePrivacyPolicy(){
     this.close.emit();
+    this.router.navigate(['/']).then(r => {});
   }
 
 }
